abd9fa4da725e37207dacf042673f813fb87a4f7
Kevin Gorham <kevin.gorham@electriccoin.co>
2020-06-06 20:20:35 -0400

    Pulled publish settings into separate file.
    So that it is easier to reuse this in our other similar projects.

--- buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
+++ buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
@@ -5,8 +5,10 @@ object Deps {
     // `Deps.Kotlin.version` directly
     const val kotlinVersion = "1.3.72"
     const val group = "cash.z.ecc.android"
+    const val artifactName = "kotlin-bip39"
     const val versionName = "1.0.0-beta08"
     const val description = "A concise implementation of BIP-0039 in Kotlin for Android."
+    const val githubUrl = "https://github.com/zcash/kotlin-bip39"
 
     object Kotlin : Version(kotlinVersion) {
         val STDLIB =        "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
