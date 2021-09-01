24e3106bc38e063aa3306b7f11d26dc05a769fde
Malachi de AElfweald <malachid@gmail.com>
2021-08-31 19:24:55 -0700

    #9 - resolve kotest failure

--- buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
+++ buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
@@ -3,13 +3,13 @@ package cash.z.ecc.android
 object Deps {
     // For use in the top-level build.gradle which gives an error when provided
     // `Deps.Kotlin.version` directly
-    const val kotlinVersion = "1.4.30"
+    const val kotlinVersion = "1.5.30"
 
     object Kotlin : Version(kotlinVersion) {
         val STDLIB =        "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
     }
 
-    object Kotest : Version("4.0.5") {
+    object Kotest : Version("4.6.1") {
         val RUNNER =        "io.kotest:kotest-runner-junit5-jvm:$version"
         val ASSERTIONS =    "io.kotest:kotest-assertions-core-jvm:$version"
         val PROPERTY =      "io.kotest:kotest-property-jvm:$version"
