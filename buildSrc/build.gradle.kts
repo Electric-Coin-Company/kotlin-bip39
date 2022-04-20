1f3a9e821d9aacea9fd4c202202565f226615771
Carter Jernigan <git@carterjernigan.com>
2022-04-20 15:19:25 -0400

    [#15] Kotlin 1.6.21
    Also update the build scripts to resolve warnings

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,15 @@
 plugins {
-    `kotlin-dsl`
+    `kotlin-dsl-base`
 }
 
 repositories {
     jcenter()
+}
+
+// Per conversation in the KotlinLang Slack, Gradle uses Java 8 compatibility internally
+// for all build scripts.
+// https://kotlinlang.slack.com/archives/C19FD9681/p1636632870122900?thread_ts=1636572288.117000&cid=C19FD9681
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
--- buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
+++ buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
@@ -3,7 +3,7 @@ package cash.z.ecc.android
 object Deps {
     // For use in the top-level build.gradle which gives an error when provided
     // `Deps.Kotlin.version` directly
-    const val kotlinVersion = "1.4.30"
+    const val kotlinVersion = "1.6.21"
 
     object Kotlin : Version(kotlinVersion) {
         val STDLIB =        "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
