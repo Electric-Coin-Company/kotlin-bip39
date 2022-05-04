bdda959f18d0d5f5fad5c1182e2c16d22f51f2b6
Carter Jernigan <git@carterjernigan.com>
2022-05-04 14:55:09 -0400

    [#22] Configure Detekt
     - Followup issue #46
    
    Added a baseline file to suppress warnings for existing issues, while enabling Detekt to run on CI so that new issues won't be introduced going forward.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,22 @@ dependencyLocking {
 
 plugins {
     id("bip39.ktlint-conventions")
+    alias(libs.plugins.detekt)
+}
+
+tasks {
+    register("detektAll", io.gitlab.arturbosch.detekt.Detekt::class) {
+        parallel = true
+        setSource(files(projectDir))
+        include("**/*.kt")
+        //include("**/*.kts")
+        exclude("**/resources/**")
+        exclude("**/build/**")
+        exclude("**/commonTest/**")
+        exclude("**/jvmTest/**")
+        exclude("**/androidTest/**")
+        config.setFrom(files("${rootProject.projectDir}/tools/detekt.yml"))
+        baseline.set(file("$rootDir/tools/detekt-baseline.xml"))
+        buildUponDefaultConfig = true
+    }
 }
