75d63886fa3eea99a3b4c497c7407d11dda7a1d4
Carter Jernigan <git@carterjernigan.com>
2022-07-18 08:06:48 -0400

    [#52] Fix Maven deployment

--- bip39-lib/build.gradle.kts
+++ bip39-lib/build.gradle.kts
@@ -1,19 +1,12 @@
+import java.util.Base64
+
 plugins {
     id("org.jetbrains.kotlin.multiplatform")
-    id("java-library")
     alias(libs.plugins.dokka)
-    alias(libs.plugins.publish)
     id("bip39.kotlin-multiplatform-conventions")
     id("bip39.dependency-conventions")
-}
-
-project.group = project.property("GROUP").toString()
-
-val libraryVersion = project.property("VERSION_NAME").toString()
-project.version = if (project.property("IS_SNAPSHOT").toString().toBoolean()) {
-    "$version-SNAPSHOT"
-} else {
-    version
+    id("maven-publish")
+    id("signing")
 }
 
 kotlin {
@@ -22,6 +15,7 @@ kotlin {
             useJUnitPlatform()
         }
     }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -47,4 +41,110 @@ kotlin {
             }
         }
     }
+}
+
+tasks {
+    val dokkaOutputDir = File(buildDir, "dokka")
+
+    dokkaHtml.configure {
+        outputDirectory.set(dokkaOutputDir)
+    }
+
+    val deleteDokkaOutputDir = register<Delete>("deleteDokkaOutputDirectory") {
+        delete(dokkaOutputDir)
+    }
+
+    register<Jar>("javadocJar") {
+        dependsOn(deleteDokkaOutputDir, dokkaHtml)
+        archiveClassifier.set("javadoc")
+        from(dokkaOutputDir)
+    }
+}
+
+val publicationVariant = "release"
+val myVersion = project.property("LIBRARY_VERSION").toString()
+val myArtifactId = "kotlin-bip39"
+val isSnapshot = project.property("IS_SNAPSHOT").toString().toBoolean()
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            artifact(tasks.getByName("javadocJar"))
+
+            artifactId = myArtifactId
+            groupId = "cash.z.ecc.android"
+            version = if (isSnapshot) {
+                "$myVersion-SNAPSHOT"
+            } else {
+                myVersion
+            }
+
+            pom {
+                name.set("Kotlin BIP-39")
+                description.set("A concise implementation of BIP-0039 in Kotlin.")
+                url.set("https://github.com/zcash/kotlin-bip39/")
+                inceptionYear.set("2020")
+                scm {
+                    url.set("https://github.com/zcash/kotlin-bip39/")
+                    connection.set("scm:git:git://github.com/zcash/kotlin-bip39.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/zcash/kotlin-bip39.git")
+                }
+                developers {
+                    developer {
+                        id.set("zcash")
+                        name.set("Zcash")
+                        url.set("https://github.com/zcash/")
+                    }
+                }
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        val mavenUrl = if (isSnapshot) {
+            project.property("ZCASH_MAVEN_PUBLISH_SNAPSHOT_URL").toString()
+        } else {
+            project.property("ZCASH_MAVEN_PUBLISH_RELEASE_URL").toString()
+        }
+        val mavenPublishUsername = project.property("ZCASH_MAVEN_PUBLISH_USERNAME").toString()
+        val mavenPublishPassword = project.property("ZCASH_MAVEN_PUBLISH_PASSWORD").toString()
+
+        mavenLocal {
+            name = "MavenLocal"
+        }
+        maven(mavenUrl) {
+            name = "MavenCentral"
+            credentials {
+                username = mavenPublishUsername
+                password = mavenPublishPassword
+            }
+        }
+    }
+}
+
+signing {
+    // Although signing is decoupled from snapshot status, note that Maven Central requires signing for non-snapshots.
+    isRequired = properties["IS_RELEASE_SIGNING_ENABLED"].toString().toBoolean()
+
+    val signingKey = run {
+        val base64EncodedKey = project.property("ZCASH_ASCII_GPG_KEY").toString()
+        if (base64EncodedKey.isNotEmpty()) {
+            val keyBytes = Base64.getDecoder().decode(base64EncodedKey)
+            String(keyBytes)
+        } else {
+            ""
+        }
+    }
+
+    if (signingKey.isNotEmpty()) {
+        useInMemoryPgpKeys(signingKey, "")
+    }
+
+    sign(publishing.publications)
 }
