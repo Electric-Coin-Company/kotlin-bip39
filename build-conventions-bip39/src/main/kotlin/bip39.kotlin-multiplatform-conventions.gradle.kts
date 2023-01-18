0e4e7a0e0fb6a48aef6b3f4084e77dd582f84070
Carter Jernigan <git@carterjernigan.com>
2023-01-18 11:58:36 -0500

    [#144] Kotlin 1.8 (#148)

--- build-conventions-bip39/src/main/kotlin/bip39.kotlin-multiplatform-conventions.gradle.kts
+++ build-conventions-bip39/src/main/kotlin/bip39.kotlin-multiplatform-conventions.gradle.kts
@@ -1,30 +1,19 @@
 pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
     extensions.findByType<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension>()?.apply {
-        jvmToolchain {
-            val javaVersion = JavaVersion.toVersion(project.property("JVM_TOOLCHAIN").toString())
-            val javaLanguageVersion = JavaLanguageVersion.of(javaVersion.majorVersion)
-            languageVersion.set(javaLanguageVersion)
-        }
-
-        targets.matching { it.platformType.name == "jvm" }.all {
-            (this as org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget).apply {
-                val javaTargetVersion = project.property("JVM_TARGET").toString()
-
-                compilations.all {
-                    kotlinOptions {
-                        jvmTarget = javaTargetVersion
-                    }
-                }
-            }
-        }
+        jvmToolchain(project.property("JVM_TOOLCHAIN").toString().toInt())
 
         targets.all {
             compilations.all {
-                kotlinOptions {
-                    allWarningsAsErrors = project.property("BIP39_IS_TREAT_WARNINGS_AS_ERRORS").toString().toBoolean()
-                    freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlin.RequiresOptIn"
-                }
+                compilerOptions.options.allWarningsAsErrors.set(
+                    project.property("BIP39_IS_TREAT_WARNINGS_AS_ERRORS").toString().toBoolean()
+                )
+
+                compilerOptions.options.freeCompilerArgs.addAll("-opt-in=kotlin.RequiresOptIn")
             }
         }
     }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(project.property("JVM_TARGET").toString().toInt())
 }
