9c228af891a1367ca0722ab239ecf4fc1aec5ebb
Carter Jernigan <git@carterjernigan.com>
2023-02-07 07:35:59 -0500

    [#150] Simplify Detekt configuration
    - Switch to configuration that builds upon default
     - Add Gradle convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,27 +9,13 @@ dependencyLocking {
 }
 
 plugins {
+    id("bip39.detekt-conventions")
     id("bip39.ktlint-conventions")
-    alias(libs.plugins.detekt)
     alias(libs.plugins.kover)
     alias(libs.plugins.versions)
 }
 
 tasks {
-    register("detektAll", io.gitlab.arturbosch.detekt.Detekt::class) {
-        parallel = true
-        setSource(files(projectDir))
-        include("**/*.kt")
-        //include("**/*.kts")
-        exclude("**/resources/**")
-        exclude("**/build/**")
-        exclude("**/commonTest/**")
-        exclude("**/jvmTest/**")
-        exclude("**/androidTest/**")
-        config.setFrom(files("${rootProject.projectDir}/tools/detekt.yml"))
-        baseline.set(file("$rootDir/tools/detekt-baseline.xml"))
-        buildUponDefaultConfig = true
-    }
 
     withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
         gradleReleaseChannel = "current"
