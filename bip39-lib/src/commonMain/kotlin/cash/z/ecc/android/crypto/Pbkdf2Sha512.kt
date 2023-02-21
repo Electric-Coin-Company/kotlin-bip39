f31a79b4e0509cba3cb770303dac23f584bcbe52
Luca Spinazzola <mspinluca@gmail.com>
2023-02-21 02:07:06 -0500

    remove implementation documentation from expect object Pbkdf2Sha512

--- bip39-lib/src/commonMain/kotlin/cash/z/ecc/android/crypto/Pbkdf2Sha512.kt
+++ bip39-lib/src/commonMain/kotlin/cash/z/ecc/android/crypto/Pbkdf2Sha512.kt
@@ -1,20 +1,5 @@
 package cash.z.ecc.android.crypto
 
-/**
- *
- * This is a clean-room implementation of PBKDF2 using RFC 2898 as a reference.
- *
- *
- * RFC 2898: http://tools.ietf.org/html/rfc2898#section-5.2
- *
- *
- * This code passes all RFC 6070 test vectors: http://tools.ietf.org/html/rfc6070
- *
- *
- * http://cryptofreek.org/2012/11/29/pbkdf2-pure-java-implementation/<br></br>
- * Modified to use SHA-512 - Ken Sedgwick ken@bonsai.com
- * Modified to for Kotlin - Kevin Gorham anothergmale@gmail.com
- */
 internal expect object Pbkdf2Sha512 {
 
     /**
