f4c4b02ab6096b19a54259c7fd7c91b3d3ee3855
Honza <rychnovsky.honza@gmail.com>
2023-09-22 11:52:14 +0200

    New Kotlin restrictions adoption

--- bip39-lib/src/commonMain/kotlin/cash/z/ecc/android/crypto/PBEKeySpecCommon.kt
+++ bip39-lib/src/commonMain/kotlin/cash/z/ecc/android/crypto/PBEKeySpecCommon.kt
@@ -3,9 +3,13 @@ package cash.z.ecc.android.crypto
 internal expect class PBEKeySpecCommon(password: CharArray?, salt: ByteArray, iterationCount: Int, keyLength: Int) {
 
     var password: CharArray?
+        private set
     var salt: ByteArray?
+        private set
     var iterationCount: Int
+        private set
     var keyLength: Int
+        private set
 
     fun clearPassword()
 }
